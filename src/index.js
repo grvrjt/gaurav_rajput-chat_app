73a12e57fd6e7c561b15c289505b58ff8e545858
GAURAV_RAJPUT <gaurav1993rajput@gmai.com>
2020-02-04 10:46:21 -0800

    format

--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ var http = require("http");
 var express = require("express");
 var socketio = require("socket.io");
 var Filter = require("bad-words");
-var {
-    generateMessage,
-    generateLocationMessage
-} = require("./utils/messages");
+var { generateMessage, generateLocationMessage } = require("./utils/messages");
 
-var { addUser,
-    removeUser,
-    getUser,
-    getUsersInRoom } = require("./utils/users");
+var { addUser, removeUser, getUser, getUsersInRoom } = require("./utils/users");
 
 var app = express();
 var server = http.createServer(app);
@@ -22,45 +16,47 @@ var publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-var count = 0;
-io.on("connection", function (socket) {
+io.on("connection", function(socket) {
     console.log("New webSocket  Connection");
 
-    socket.on("join", function (options, callback) {
-        var { error, user } = addUser({ id: socket.id, ...options });
+    socket.on("join", function(options, callback) {
+        var { error, user } = addUser({
+            id: socket.id,
+            ...options
+        });
         if (error) {
             return callback(error);
         }
         socket.join(user.room);
-        socket.emit("message", generateMessage("Admin"," welcome !"));
-        socket.broadcast.to(user.room).emit("message", generateMessage("Admin "+user.username + " has joined !"));
-        io.to(user.room).emit("roomData",{
-            room:user.room,
-            users:getUsersInRoom(user.room)
+        socket.emit("message", generateMessage("Admin", " welcome !"));
+        socket.broadcast.to(user.room).emit("message", generateMessage("Admin " + user.username + " has joined !"));
+        io.to(user.room).emit("roomData", {
+            room: user.room,
+            users: getUsersInRoom(user.room)
         });
         callback();
     });
 
-    socket.on("sendMessage", function (message, callback) {
+    socket.on("sendMessage", function(message, callback) {
         var user = getUser(socket.id);
         var filter = new Filter();
         if (filter.isProfane(message)) {
             return callback("Profanity is not allowed");
         }
-        io.to(user.room).emit("message", generateMessage(user.username,message));
+        io.to(user.room).emit("message", generateMessage(user.username, message));
         callback();
     });
 
-    socket.on("sendLocation", function (coords, callback) {
+    socket.on("sendLocation", function(coords, callback) {
         var user = getUser(socket.id);
-        io.to(user.room).emit("sendLocationMessage", generateLocationMessage(user.username,"http://google.com/maps?q=" + coords.latitude + "," + coords.longitude));
+        io.to(user.room).emit("sendLocationMessage", generateLocationMessage(user.username, "http://google.com/maps?q=" + coords.latitude + "," + coords.longitude));
         callback();
     });
 
-    socket.on("disconnect", function () {
+    socket.on("disconnect", function() {
         var user = removeUser(socket.id);
         if (user) {
-            io.to(user.room).emit('message', generateMessage("Admin "+user.username+" has left"));
+            io.to(user.room).emit('message', generateMessage("Admin " + user.username + " has left"));
             io.to(user.room).emit("roomData", {
                 room: user.room,
                 users: getUsersInRoom(user.room)
@@ -72,6 +68,6 @@ io.on("connection", function (socket) {
 
 
 
-server.listen(port, function () {
+server.listen(port, function() {
     console.log("Server  is listening at the port number 3000");
 });
